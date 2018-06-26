98ecf3f53157b7aea84296d1f72519166123822a
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2018-06-26 08:10:10 +0200

    add test RMLTC0011b-CSV

--- src/test/java/be/ugent/rml/Mapper_CSV_Test.java
+++ src/test/java/be/ugent/rml/Mapper_CSV_Test.java
@@ -140,6 +140,11 @@ public class Mapper_CSV_Test extends TestCore {
     }
 
     @Test
+    public void evaluate_0011b_CSV() {
+        doMapping("./test-cases/RMLTC0011b-CSV/mapping.ttl", "./test-cases/RMLTC0011b-CSV/output.ttl");
+    }
+
+    @Test
     public void evaluate_1003_CSV() {
         doMapping("./test-cases/RMLTC1003-CSV/mapping.ttl", "./test-cases/RMLTC1003-CSV/output.ttl");
     }
