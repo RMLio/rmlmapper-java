0f58301ef0b9bca0d6689204b31ed3d9b9926e99
Zeno Dhaene <zeno.dhaene@gmail.com>
2018-07-16 13:56:03 +0200

    Added test cases

--- src/test/java/be/ugent/rml/Mapper_JSON_Test.java
+++ src/test/java/be/ugent/rml/Mapper_JSON_Test.java
@@ -152,4 +152,9 @@ public class Mapper_JSON_Test extends TestCore {
     public void evaluate_0012b_JSON() {
         doMapping("./test-cases/RMLTC0012b-JSON/mapping.ttl", "./test-cases/RMLTC0012b-JSON/output.ttl");
     }
+
+    @Test
+    public void evaluate_0013_JSON() {
+        doMapping("./test-cases/RMLTC0013-JSON/mapping.ttl", "./test-cases/RMLTC0013-JSON/output.nt");
+    }
 }
