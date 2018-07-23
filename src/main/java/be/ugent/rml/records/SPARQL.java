8d882a127ff057d1fc5485bea3cd21ee76f085b4
Dries Marzougui <dries.marzougui@gmail.com>
2018-07-23 16:54:48 +0200

    cleanup

--- src/main/java/be/ugent/rml/records/SPARQL.java
+++ src/main/java/be/ugent/rml/records/SPARQL.java
@@ -1,52 +1,32 @@
 package be.ugent.rml.records;
 
-import com.jayway.jsonpath.Configuration;
-import com.jayway.jsonpath.JsonPath;
-import com.jayway.jsonpath.Option;
-import com.jayway.jsonpath.PathNotFoundException;
 import org.apache.jena.query.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
 
 public class SPARQL extends IteratorFormat {
 
     @Override
     public List<Record> get(String endpoint, String qs, String iterator) {
-        List<Record> records = new ArrayList<>();
         // Query the endpoint
         Query query = QueryFactory.create(qs);
         System.out.println(qs);
         QueryExecution exec = QueryExecutionFactory.sparqlService( endpoint, qs);
         ResultSet results = exec.execSelect();
 
-        // Convert resultset to JSON
+        // Convert ResultSet to JSON
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         ResultSetFormatter.outputAsJSON(outputStream, results);
 
-        Object document = Configuration.defaultConfiguration().jsonProvider().parse(new ByteArrayInputStream(outputStream.toByteArray()), "utf-8");
-
-        Configuration conf = Configuration.builder()
-                .options(Option.AS_PATH_LIST).build();
-
-        try {
-            List<String> pathList = JsonPath.using(conf).parse(document).read(iterator);
-            for(String p :pathList) {
-                records.add(new JSONRecord(document, p));
-            }
-        } catch(PathNotFoundException e) {
-            logger.warn(e.getMessage(), e);
-        }
-
-        return records;
+        return _get(new ByteArrayInputStream(outputStream.toByteArray()), iterator);
     }
 
     @Override
-    List<Record> _get(InputStream stream, String iterator) throws IOException {
-        return null;
+    List<Record> _get(InputStream stream, String iterator) {
+        JSON json = new JSON();
+        return json._get(stream, iterator);
     }
 }
--- src/test/java/be/ugent/rml/Mapper_SPARQL_Test.java
+++ src/test/java/be/ugent/rml/Mapper_SPARQL_Test.java
@@ -1,9 +1,10 @@
 package be.ugent.rml;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class Mapper_SPARQL_Test extends TestCore {
-    @Test
+    @Ignore
     public void evaluate_example() {
         doMapping("./test-cases/example-SPARQL/mapping.ttl", "./test-cases/example-SPARQL/output.ttl");
     }
