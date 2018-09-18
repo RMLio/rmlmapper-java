7092606d793cf1521e4fe095fcf45e10f9d5ea2e
Ben De Meester <ben.demeester@ugent.be>
2018-09-18 15:42:42 +0200

    eat sleep test repeat

--- src/test/java/be/ugent/rml/Arguments_Test.java
+++ src/test/java/be/ugent/rml/Arguments_Test.java
@@ -3,35 +3,44 @@ package be.ugent.rml;
 import be.ugent.rml.cli.Main;
 import org.junit.Test;
 
+import java.io.File;
+import static org.junit.Assert.assertTrue;
+
 public class Arguments_Test extends TestCore {
 
     @Test
     public void withConfigFile() {
         Main.main("-c ./ARGUMENT_TEST_config_file/config_example.properties".split(" "));
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         compareFiles(
                 "./ARGUMENT_TEST_config_file/target_output.nq",
-                "./ARGUMENT_TEST_config_file/generated_output.nq",
+                "./generated_output.nq",
                 false
         );
+
+        File outputFile = null;
+        try {
+            outputFile = Utils.getFile("./generated_output.nq");
+            assertTrue(outputFile.delete());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void withoutConfigFile() {
-        Main.main("-m ./ARGUMENT_TEST_config_file/mapping.ttl -o ./src/test/resources/ARGUMENT_TEST_config_file/generated_output.nq".split(" "));
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Main.main("-m ./ARGUMENT_TEST_config_file/mapping.ttl -o ./generated_output.nq".split(" "));
         compareFiles(
                 "./ARGUMENT_TEST_config_file/target_output.nq",
-                "./ARGUMENT_TEST_config_file/generated_output.nq",
+                "./generated_output.nq",
                 false
         );
+
+        File outputFile = null;
+        try {
+            outputFile = Utils.getFile("./generated_output.nq");
+            assertTrue(outputFile.delete());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
