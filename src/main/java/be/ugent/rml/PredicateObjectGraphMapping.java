49612a82759b43c06fa925e84f65ad4bd19e39c4
Ben De Meester <ben.demeester@ugent.be>
2019-05-16 11:35:01 +0200

    removed useless class

--- src/main/java/be/ugent/rml/PredicateObjectGraphMapping.java
+++ src/main/java/be/ugent/rml/PredicateObjectGraphMapping.java
@@ -1,6 +1,5 @@
 package be.ugent.rml;
 
-import be.ugent.rml.functions.JoinConditionFunctionExecutor;
 import be.ugent.rml.functions.MultipleRecordsFunctionExecutor;
 import be.ugent.rml.term.Term;
 
