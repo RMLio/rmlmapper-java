11fd0fae2434ba075caab26f37d9419858c27f70
Dylan Van Assche <dylan.vanassche@ugent.be>
2021-07-01 16:57:49 +0200

    NAMESPACES: fix W3C formats namespace
    Namespace uses 'http' instead 'https'

--- src/main/java/be/ugent/rml/NAMESPACES.java
+++ src/main/java/be/ugent/rml/NAMESPACES.java
@@ -28,6 +28,6 @@ public class NAMESPACES {
     public static final String WOTSEC = "https://www.w3.org/2019/wot/security#";
     public static final String IDSA = "https://w3id.org/idsa/core/";
     public static final String RMLT = "http://semweb.mmlab.be/ns/rml-target#";
-    public static final String FORMATS = "https://www.w3.org/ns/formats/";
+    public static final String FORMATS = "http://www.w3.org/ns/formats/";
     public static final String COMP = "http://semweb.mmlab.be/ns/rml-compression#";
 }
