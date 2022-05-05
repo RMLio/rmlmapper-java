5e0ea121f7d2fede722faaf783ac9c254b63eb9c
Julian Rojas <julian.rojas87@gmail.com>
2022-05-05 16:43:59 +0200

    XMLRecordFactory: Change to handle XPath expressions with the Saxon parser - Adjust XMLRecord class to use Saxon - Add SaxNamespaceResolver class for Saxon. This fixes [#154](https://github.com/RMLio/rmlmapper-java/issues/154)

--- src/main/java/be/ugent/rml/records/XMLRecord.java
+++ src/main/java/be/ugent/rml/records/XMLRecord.java
@@ -3,16 +3,10 @@ package be.ugent.rml.records;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-import be.ugent.rml.records.xpath.NamespaceResolver;
-
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
+import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.XPathCompiler;
+import net.sf.saxon.s9api.XdmItem;
+import net.sf.saxon.s9api.XdmValue;
 
 /**
  * This class is a specific implementation of a record for XML.
@@ -20,10 +14,13 @@ import javax.xml.xpath.XPathFactory;
  */
 public class XMLRecord extends Record {
 
-    private Node node;
+    private XdmItem item;
+    private XPathCompiler compiler;
 
-    public XMLRecord(Node node) {
-        this.node = node;
+    public XMLRecord(XdmItem item, XPathCompiler compiler) {
+        this.item = item;
+        // Keep a reference to the XPath compiler for faster future queries
+        this.compiler = compiler;
     }
 
     /**
@@ -35,21 +32,14 @@ public class XMLRecord extends Record {
     @Override
     public List<Object> get(String value) {
         List<Object> results = new ArrayList<>();
-        XPath xPath = XPathFactory.newInstance().newXPath();
-        xPath.setNamespaceContext(new NamespaceResolver(this.node.getOwnerDocument()));
+        
         try {
-            NodeList result = (NodeList) xPath.compile(value).evaluate(node, XPathConstants.NODESET);
-
-            for (int i = 0; i < result.getLength(); i++) {
-                results.add(result.item(i).getTextContent());
-            }
-        } catch (XPathExpressionException e1) {
-            /* Fallback to string representation if not a nodeset. */
-            try {
-                results.add((String) xPath.compile(value).evaluate(node));
-            } catch (XPathExpressionException e2) {
-                e2.printStackTrace();
-            }
+            XdmValue result = compiler.evaluate(value, item);
+            result.forEach((node) -> {
+                results.add(node.getStringValue());  
+            });
+        } catch (SaxonApiException e1) {
+            e1.printStackTrace();
         }
 
 
