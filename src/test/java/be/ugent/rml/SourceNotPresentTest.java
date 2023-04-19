790c5991e1a4b3636468c068813a0d84a88cc050
tristanverbeken <tristan.verbeken@ugent.be>
2023-04-19 16:05:58 +0200

    Tests for the new sources check function in executor.

---
+++ src/test/java/be/ugent/rml/SourceNotPresentTest.java
@@ -0,0 +1,34 @@
+package be.ugent.rml;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class SourceNotPresentTest extends TestFunctionCore{
+
+
+    @Test
+    public void evaluate_1035_CSV() throws Exception {
+        Executor executor = this.createExecutor("./test-cases/RMLTC1035-CSV/mapping.ttl");
+        ClassLoader classLoader = getClass().getClassLoader();
+        // execute mapping file
+        URL url = classLoader.getResource("./test-cases/RMLTC1035-CSV");
+        assert url != null;
+        assertThrows(IOException.class,() -> executor.verifySources( url.getPath()));
+    }
+
+    @Test
+    public void evaluate_1036_CSV() throws Exception {
+        Executor executor = this.createExecutor("./test-cases/RMLTC1036-CSV/mapping.ttl");
+        ClassLoader classLoader = getClass().getClassLoader();
+        // execute mapping file
+        URL url = classLoader.getResource("./test-cases/RMLTC1036-CSV");
+        assert url != null;
+        executor.verifySources( url.getPath());
+    }
+
+
+}
