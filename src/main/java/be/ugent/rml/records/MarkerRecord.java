564d4ed689f23b4106bb729c1ddf23b71ee8c6bd
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2023-10-10 09:21:04 +0200

    Optimize generating magic markers a bit
    - Executor: only initialize record & subjects after checking if magic marker generation is necessary
    - MarkerRecord:
      - Remove getDataType method since it's the same as in the superclass;
      - return IDLabFunctions.MAGIC_MARER directly.

--- src/main/java/be/ugent/rml/records/MarkerRecord.java
+++ src/main/java/be/ugent/rml/records/MarkerRecord.java
@@ -3,27 +3,13 @@ package be.ugent.rml.records;
 import be.ugent.knows.idlabFunctions.IDLabFunctions;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * This class is a specific implementation of a record for the magic marker.
  * Every record always returns the magic marker, no matter th input.
  */
 public class MarkerRecord extends Record {
-
-    private String MAGIC_MARKER = IDLabFunctions.MAGIC_MARKER;
-
-    /**
-     * This method returns the datatype of a reference in the record.
-     * @param value the reference for which the datatype needs to be returned.
-     * @return the IRI of the datatype.
-     */
-    public String getDataType(String value) {
-        return null;
-    }
-
     /**
      * This method returns the objects for a column in the CSV record (= CSV row).
      * @param value the column for which objects need to be returned.
@@ -32,7 +18,7 @@ public class MarkerRecord extends Record {
     @Override
     public List<Object> get(String value) {
         List<Object> result = new ArrayList<>();
-        result.add(MAGIC_MARKER);
+        result.add(IDLabFunctions.MAGIC_MARKER);
 
         return result;
     }
