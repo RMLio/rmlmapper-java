7ee77d7010532caaeba4670e147ec3c0a63255ab
abuzogan <aron.buzogany@ugent.be>
2023-11-16 12:48:27 +0100

    code-style: added space before curly bracket

--- src/main/java/be/ugent/rml/term/NamedNode.java
+++ src/main/java/be/ugent/rml/term/NamedNode.java
@@ -2,7 +2,7 @@ package be.ugent.rml.term;
 
 import org.eclipse.rdf4j.model.impl.SimpleIRI;
 
-public class NamedNode extends SimpleIRI implements Term{
+public class NamedNode extends SimpleIRI implements Term {
 
     public NamedNode(final String iri) {
         super(iri);
