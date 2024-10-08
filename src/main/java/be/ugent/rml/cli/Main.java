811bdfb13460b96b4a2ba0b8d3fab7bc6ecaf120
Els de Vleeschauwer <els.devleeschauwer@ugent.be>
2024-10-08 16:01:57 +0200

    Main: added options solidHelperUrl and solidHelperDocker
    These options facilitate the use of the solid target helper in combination with rmlmapper

--- src/main/java/be/ugent/rml/cli/Main.java
+++ src/main/java/be/ugent/rml/cli/Main.java
@@ -11,6 +11,7 @@ import be.ugent.rml.records.RecordsFactory;
 import be.ugent.rml.store.Quad;
 import be.ugent.rml.store.QuadStore;
 import be.ugent.rml.store.RDF4JStore;
+import be.ugent.rml.target.SolidTarget;
 import be.ugent.rml.target.Target;
 import be.ugent.rml.target.TargetFactory;
 import be.ugent.rml.term.NamedNode;
@@ -161,6 +162,17 @@ public class Main {
                 .longOpt("convert-mapping")
                 .desc("Only convert the mapping to the latest RML specification by the W3C Community Group")
                 .build();
+        // temporary solution until the oidc authentication in is implemented in JAVA
+        Option solidHelperUrlOption = Option.builder("shu")
+                .longOpt("solidHelperUrl")
+                .desc("the url on which the javascript helper application for Solid targets can be reached, default http://localhost:8080/")
+                .hasArg()
+                .build();
+        // temporary solution until the oidc authentication in is implemented in JAVA
+        Option solidHelperDockerOption = Option.builder("shd")
+                .longOpt("solidHelperDocker")
+                .desc("select this option when the docker with solid target helper inside RMLMapper is needed")
+                .build();
 
         options.addOption(mappingdocOption);
         options.addOption(privateSecurityDataOption);
@@ -181,6 +193,8 @@ public class Main {
         options.addOption(baseIriOption);
         options.addOption(provideOwnEOFMarkerOption);
         options.addOption(convertMapping);
+        options.addOption(solidHelperUrlOption);
+        options.addOption(solidHelperDockerOption);
 
         CommandLineParser parser = new DefaultParser();
         try {
@@ -454,7 +468,11 @@ public class Main {
 
                 IDLabFunctions.saveState();
 
-                writeOutputTargets(targets, rmlStore, basePath, outputFile, outputFormat);
+                String solidHelperUrl = getPriorityOptionValue(solidHelperUrlOption, lineArgs, configFile, "http://localhost:8080/");
+                Boolean solidHelperDocker = checkOptionPresence(solidHelperDockerOption, lineArgs, configFile);
+
+                writeOutputTargets(targets, rmlStore, basePath, outputFile, outputFormat, solidHelperUrl, solidHelperDocker);
+
             }
             // Get stop timestamp for post mapping metadata
             String stopTimestamp = Instant.now().toString();
@@ -477,7 +495,7 @@ public class Main {
         }
     }
 
-    private static void writeOutputTargets(Map<Term, QuadStore> targets, QuadStore rmlStore, String basePath, String outputFileDefault, String outputFormatDefault) throws Exception {
+    private static void writeOutputTargets(Map<Term, QuadStore> targets, QuadStore rmlStore, String basePath, String outputFileDefault, String outputFormatDefault, String solidHelperUrl, Boolean solidHelperDocker) throws Exception {
         boolean hasNoResults = true;
 
         logger.debug("Writing to Targets: {}", targets.keySet());
@@ -513,13 +531,17 @@ public class Main {
             }
             else {
                 logger.debug("Exporting to Target: {}", term);
+                Target target = targetFactory.getTarget(term, rmlStore, store);
+                if((target instanceof SolidTarget)) {
+                    ((SolidTarget) target).setSolidHelperUrl(solidHelperUrl);
+                    ((SolidTarget) target).setSolidHelperDocker(solidHelperDocker);
+                }
                 if (store.size() > 1) {
                     logger.info("{} quads were generated for {} Target", store.size(), term);
                 } else {
                     logger.info("{} quad was generated {} Target", store.size(), term);
                 }
 
-                Target target = targetFactory.getTarget(term, rmlStore, store);
                 String serializationFormat = target.getSerializationFormat();
                 OutputStream output = target.getOutputStream();
                 store.addQuads(target.getMetadata());
@@ -555,6 +577,11 @@ public class Main {
         } else {
             return null;
         }
+    }
+
+    private static String getPriorityOptionValue(Option option, CommandLine lineArgs, Properties properties, String defaultValue) {
+        String value =  getPriorityOptionValue(option, lineArgs, properties);
+        return value == null ? defaultValue : value;
     }
 
     private static String[] getOptionValues(Option option, CommandLine lineArgs, Properties properties) {
