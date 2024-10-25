c83d4fd3b268112bccba6a510dbdb6b8e287a934
Els de Vleeschauwer <els.devleeschauwer@ugent.be>
2024-10-25 21:52:53 +0200

    SolidTargetTest: using solid playground for Solid related containers
    Using a docker with CSS testpods is very complex: no fixed hostname and hostport with testcontainers, therefore all URI in the test data needs to be adapted when running the tests.

--- src/test/java/be/ugent/rml/SolidTargetTest.java
+++ src/test/java/be/ugent/rml/SolidTargetTest.java
@@ -4,25 +4,17 @@ import be.ugent.rml.cli.Main;
 import be.ugent.rml.store.QuadStore;
 import be.ugent.rml.store.QuadStoreFactory;
 import be.ugent.rml.target.SolidTargetHelper;
-import com.github.dockerjava.api.model.ExposedPort;
-import com.github.dockerjava.api.model.HostConfig;
-import com.github.dockerjava.api.model.PortBinding;
-import com.github.dockerjava.api.model.Ports;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.utility.DockerImageName;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.Duration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class SolidTargetTest extends TestCore {
@@ -31,63 +23,63 @@ public class SolidTargetTest extends TestCore {
     @Test
     public void solid1() throws Exception {
         doMappingSolid("solid-target/solid1/mapping.ttl",
-                "http://localhost:3000/example/building",
+                "https://pod.playground.solidlab.be/user1/rmlmapper/building",
                 "solid-target/solid1/output1.nq",
-                "example");
+                "user1");
     }
 
     //constant target in predicate map
     @Test
     public void solid2() throws Exception {
         doMappingSolid("solid-target/solid2/mapping.ttl",
-                "http://localhost:3000/example/building",
+                "https://pod.playground.solidlab.be/user1/rmlmapper/building",
                 "solid-target/solid2/output1.nq",
-                "example");
+                "user1");
     }
 
     //constant target in object map
     @Test
     public void solid3() throws Exception {
         doMappingSolid("solid-target/solid3/mapping.ttl",
-                "http://localhost:3000/example/building",
+                "https://pod.playground.solidlab.be/user1/rmlmapper/building",
                 "solid-target/solid3/output1.nq",
-                "example");
+                "user1");
     }
 
     //constant target in subject graph
     @Test
     public void solid4() throws Exception {
         doMappingSolid("solid-target/solid4/mapping.ttl",
-                "http://localhost:3000/example/building",
+                "https://pod.playground.solidlab.be/user1/rmlmapper/building",
                 "solid-target/solid4/output1.nq",
-                "example");
+                "user1");
     }
 
     //constant target in po graph
     @Test
     public void solid5() throws Exception {
         doMappingSolid("solid-target/solid5/mapping.ttl",
-                "http://localhost:3000/example/building",
+                "https://pod.playground.solidlab.be/user1/rmlmapper/building",
                 "solid-target/solid5/output1.nq",
-                "example");
+                "user1");
     }
 
     //acl for pod1, resourceUrl with .ttl
     @Test
     public void acl1() throws Exception{
         doMappingSolid("solid-target/acl1/mapping.ttl",
-                "http://localhost:3000/example/building.ttl",
+                "https://pod.playground.solidlab.be/user1/rmlmapper/building.ttl",
                 "solid-target/acl1/output1.nq",
-                "pod1");
+                "user2");
     }
 
     //acl for pod1, resourceUrl without .ttl
     @Test
     public void acl2() throws Exception{
         doMappingSolid("solid-target/acl2/mapping.ttl",
-                "http://localhost:3000/example/building",
+                "https://pod.playground.solidlab.be/user1/rmlmapper/building",
                 "solid-target/acl2/output1.nq",
-                "pod1");
+                "user2");
     }
 
     void doMappingSolid(String mapPath, String resourceUrl, String outPath, String user) throws Exception {
@@ -95,48 +87,24 @@ public class SolidTargetTest extends TestCore {
     }
 
     void doMappingSolid(String mapPath, String[] resourceUrls, String[] outPaths, String[] users) throws Exception {
-        // We need a fixed host port because the port is part of the pod uri (also of the webid, acl file)
-        // Test containers advise against the use of a fixed host port, and deprecated the related solutions
-        // Finally, found a workaround here: https://github.com/testcontainers/testcontainers-java/issues/256
-        int port = 3000;
-        PortBinding portBinding = new PortBinding(Ports.Binding.bindPort(port), new ExposedPort(port));
-        try (GenericContainer<?> container = new GenericContainer<>(DockerImageName.parse("elsdvlee/solid-testpods:latest"))
-                .withCreateContainerCmdModifier(cmd -> cmd.withHostConfig(new HostConfig().withPortBindings(portBinding)))
-                .withExposedPorts(port)
-                .withCommand("npm", "start")
-                .waitingFor(Wait.forHealthcheck()).withStartupTimeout(Duration.ofSeconds(200))){
-            container.start();
-            String host = container.getHost();
-            final String hostURL = "http://" + host + ":" + port;
-            logger.info("*** Solid Docker container runs on {}", hostURL);
-
-            // replace URLs in mapping file, write to temporary file, and pass that to mapper.
-            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-            String realMapPath = classLoader.getResource(mapPath).getPath();
-            String mappingFileContents = Files.readString(Path.of(realMapPath), StandardCharsets.UTF_8);
-            String correctedMappingFileContents = mappingFileContents.replaceAll("@@HOSTPORT@@", hostURL);
-            File tempMappingFile = File.createTempFile("solidTargetTestMapping", ".ttl");
-            tempMappingFile.deleteOnExit();
-            String tmpMappingPath = tempMappingFile.getAbsolutePath();
-            Files.writeString(Path.of(tmpMappingPath), correctedMappingFileContents, StandardCharsets.UTF_8);
-
-            Main.run(("-m " + tmpMappingPath).split(" "));
-            int i = 0;
-            while (i < resourceUrls.length) {
-                Map<String, String> solidTargetInfo = getSolidTargetInfo(users[i], resourceUrls[i], hostURL);
-                compareResourceWithOutput(outPaths[i], solidTargetInfo);
-                i++;
-            }
+        Main.run(("-m " + mapPath).split(" "));
+        SolidTargetHelper helper = new SolidTargetHelper();
+        int i = 0;
+        while (i < resourceUrls.length) {
+            Map<String, String> solidTargetInfo = getSolidTargetInfo(users[i], resourceUrls[i]);
+            compareResourceWithOutput(outPaths[i], solidTargetInfo);
+            helper.deleteResource(getSolidTargetInfo("user1", resourceUrls[i]));
+            i++;
         }
     }
 
     // get solidTargetInfo including authentication details of testpods
-    private Map<String, String> getSolidTargetInfo(String user, String resourceUrl, String hostURL){
+    private Map<String, String> getSolidTargetInfo(String user, String resourceUrl){
         Map<String, String> solidTargetInfo = new HashMap<>();
-        solidTargetInfo.put("email", "hello@" + user + ".com");
-        solidTargetInfo.put("password","abc123");
-        solidTargetInfo.put("serverUrl", hostURL + "/");
-        solidTargetInfo.put("webId", hostURL + "/" + user + "/profile/card#me");
+        solidTargetInfo.put("email", user + "@pod.playground.solidlab.be");
+        solidTargetInfo.put("password",user);
+        solidTargetInfo.put("serverUrl", "https://pod.playground.solidlab.be/");
+        solidTargetInfo.put("webId", "https://pod.playground.solidlab.be/" + user + "/profile/card#me");
         solidTargetInfo.put("resourceUrl", resourceUrl);
         return solidTargetInfo;
     }
@@ -151,5 +119,16 @@ public class SolidTargetTest extends TestCore {
         result.removeDuplicates();
         compareStores(filePathToStore(outPath), result);
     }
-}
 
+    private static void replaceStringInFile(Path filePath, String target, String replacement) {
+        try {
+            List<String> lines = Files.readAllLines(filePath, StandardCharsets.UTF_8);
+            for (int i = 0; i < lines.size(); i++) {
+                lines.set(i, lines.get(i).replace(target, replacement));
+            }
+            Files.write(filePath, lines, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
