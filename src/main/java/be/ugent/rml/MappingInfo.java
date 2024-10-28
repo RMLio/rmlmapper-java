d8167ed6d57d61f67385a9356240e03fea15b933
Els de Vleeschauwer <els.devleeschauwer@ugent.be>
2024-10-28 15:35:13 +0100

    MappingInfo: added targetGenerator

--- src/main/java/be/ugent/rml/MappingInfo.java
+++ src/main/java/be/ugent/rml/MappingInfo.java
@@ -11,23 +11,27 @@ public class MappingInfo {
     private final Term term;
     private final TermGenerator termGenerator;
     private final List<Term> targets;
+    private List<TermGenerator> targetGenerators;
 
-    public MappingInfo(@Nonnull Term term, @Nonnull TermGenerator termGenerator, @Nonnull List<Term> targets) {
+    public MappingInfo(@Nonnull Term term, @Nonnull TermGenerator termGenerator, @Nonnull List<Term> targets, List<TermGenerator> targetGenerators) {
         this.term = term;
         this.termGenerator = termGenerator;
         this.targets = targets;
+        this.targetGenerators = targetGenerators;
     }
 
     public MappingInfo(@Nonnull Term term, @Nonnull TermGenerator termGenerator) {
         this.term = term;
         this.termGenerator = termGenerator;
         this.targets = Collections.emptyList();
+        this.targetGenerators = Collections.emptyList();
     }
 
-    public MappingInfo(@Nonnull Term term, @Nonnull List<Term> targets) {
+    public MappingInfo(@Nonnull Term term, @Nonnull List<Term> targets, List<TermGenerator> targetGenerators) {
         this.term = term;
         this.termGenerator = null;
         this.targets = targets;
+        this.targetGenerators = targetGenerators;
     }
 
     public Term getTerm() {
@@ -42,7 +46,15 @@ public class MappingInfo {
         return targets;
     }
 
+    public List<TermGenerator> getTargetGenerators() {
+        return this.targetGenerators;
+    }
+
     public void addTargets(List<Term> targets) {
         this.targets.addAll(targets);
+    }
+
+    public void addTargetGenerators(List<TermGenerator> targetGenerators) {
+        getTargetGenerators().addAll(targetGenerators);
     }
 }
